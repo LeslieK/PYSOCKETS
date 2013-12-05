d4403e4305942b4c6effcb06205e01ceba7e4238
LeslieK <leslieklein@comcast.net>
2013-12-05 11:27:50 -0500

    initial client.py, server.py files

--- client.py
+++ client.py
@@ -1,19 +1,21 @@
 import socket
 # CLIENT
 MAX_BUFFER_SIZE = 16
+PORT = 8765
 
-class mysocket:
-    '''demonstration class only
-    - coded for clarity, not efficiency
+class MySocket:
+    '''
+    a client socket
     '''
 
     def __init__(self, sock=None):
-        self.msg2_start = ''
+        '''creates a client socket'''
         if sock is None:
             self.sock = socket.socket(
                 socket.AF_INET, socket.SOCK_STREAM)
         else:
             self.sock = sock
+        self.msg2_start = '' # beginning of msg2; transmitted with end of msg1
 
     def connect(self, host, port):
         self.sock.connect((host, port))
@@ -26,37 +28,45 @@ class mysocket:
             if sent == 0:
                 raise RuntimeError("socket connection broken")
             totalsent = totalsent + sent
+        # add delimiter
         self.sock.send("\0")
 
     
     def myreceive_one(self):
+        '''read one msg, chunk by chunk'''
         msg = self.msg2_start
+        self.msg2_start = '' # reset instance variable
         while True:
             chunk = self.sock.recv(MAX_BUFFER_SIZE)
             print ("client received ", chunk, len(chunk))
+            # check for msg delimiter
             index = chunk.find("\0")
             if index  >= 0:
+                # delimiter found
                 msg1_end, msg2_start = chunk.split('\0')
                 self.msg2_start = msg2_start
                 msg = msg + msg1_end
                 return msg
             else:
+                # delimiter not found
                 msg = msg + chunk
 
             
     def myreceive_all(self):
         msgs=[]
         first_msg = self.myreceive_one()
-        print ("first msg:", first_msg)
+        print ("first msg:", first_msg) # number of msgs to follow; sent by server
         num_msgs = int(first_msg)
         for i in range(num_msgs):
             msgs.append(self.myreceive_one())
-            print ("msgs", msgs)
+            print ("msg", msgs[i])
         self.sock.close()
-        return msgs
+        text = ''.join(msgs)
+        print len(text)
+        return text
 
-s = mysocket()
-s.connect('', 8765)
+s = MySocket()
+s.connect('', PORT)
 s.mysend('Here I am. Sending a longer message.')
 print (s.myreceive_all())
 
--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 import socket
-# SERVER !!!
+# SERVER 
 
 MAX_BUF_SIZE = 1024
+PORT = 8765
 
-class mysocket:
-    '''demonstration class only
-    - coded for clarity, not efficiency
+class MySocket:
+    '''
+    a server socket
     '''
 
     def __init__(self, sock=None):
@@ -18,19 +19,21 @@ class mysocket:
 
     def serve(self, port, handler):
         self.sock.bind(('', port))
-        self.sock.listen(5)
+        self.sock.listen(5) # queue backlog = 5
         while(True):
             client_sock, client_addr = self.sock.accept()
             print ("connection attempt", client_addr)
             handler(client_addr, client_sock)
 
-def my_handler(address, client_sock):
+def my_handler(client_addr, client_sock):
     while True:
         msg = client_sock.recv(MAX_BUF_SIZE)
         print ("received ",  len(msg))
         if not msg:
             break
+        # send msg with number of msgs to expect after this one
         client_sock.sendall("3\0")
+        # send msg 1
         client_sock.sendall(msg)
         client_sock.sendall("Another message!\0")
         client_sock.sendall("That's all folks!\0")
@@ -38,4 +41,4 @@ def my_handler(address, client_sock):
     print ("Connection closed. Bye")
     
 s = mysocket()
-s.serve(8765, my_handler)
+s.serve(PORT, my_handler)
